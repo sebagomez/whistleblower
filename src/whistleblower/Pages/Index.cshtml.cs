3e2f6fe54547300caddde4999b2bd99040627fb3
Sebastián Gómez <sebastiangomezcorrea@gmail.com>
2022-02-22 15:36:19 -0500

    Add version to the web ui and as docker image tag

--- src/whistleblower/Pages/Index.cshtml.cs
+++ src/whistleblower/Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ namespace whistleblower.Pages;
 public class IndexModel : PageModel
 {
     public List<EnvVar> Variables { get; set; } = new List<EnvVar>();
+
+    public string Version { get; set; }
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +16,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        Version? assVersion = typeof(IndexModel).Assembly.GetName().Version;
+        Version = assVersion is not null ? $"{assVersion.Major}.{assVersion.Minor}.{assVersion.Build}" : "";
     }
 }
